740f6e154d4591eed5d4983ceccaed4b315e14a2
PurnimaNaik <naikpurnima9@gmail.com>
2019-09-01 19:03:31 -0400

    Initial setup commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+var path= require("path");
+
+var DIST_DIR=path.resolve(__dirname, "dist");
+var SRC_DIR=path.resolve(__dirname, "src");
+
+var config={
+  entry: SRC_DIR + "/app/index.js",
+  mode: 'development',
+  output: {
+    path:DIST_DIR + "/app",
+    filename: "bundle.js",
+    publicPath: "/app/"
+  },
+
+  module:{
+  rules: [
+    {
+      test: /\.js?/,
+      include: SRC_DIR,
+      loader:"babel-loader",
+      query: {
+        presets: ['@babel/react'],
+      }
+    },
+    {
+    test: /\.css$/,
+    use: [
+      { loader: 'style-loader' },
+      { loader: 'css-loader' }
+    ]
+  },
+  ]
+  }
+
+};
+
+module.exports=config;
