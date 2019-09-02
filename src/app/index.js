b4f9e6095862cab6fd8f4ccc384253f2d4f8a528
PurnimaNaik <naikpurnima9@gmail.com>
2019-09-01 20:17:11 -0400

    API call added

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,55 @@
-console.log("wooooooorrrkkkss");
+
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            packageName:"",
+            startDate:"",
+            endDate:""
+        };
+    }
+ 
+    componentDidMount(){
+        // this.retrieveCount();
+    }
+
+    retrieveCount(){
+        var request = new XMLHttpRequest();
+        // https://api.npmjs.org/downloads/point/2019-06-01:2019-09-30/two-part-progress-circle
+        request.open('GET', 'https://api.npmjs.org/downloads/point/2019-06-01:2019-09-30/two-part-progress-circle', true);
+        request.onload = function () {
+          var data = JSON.parse(request.response);
+
+          if (request.readyState == 4 && request.status == 200){
+            this.oncallback(data);
+          }
+          else if(request.readyState == 4 && request.status == 404){
+            this.onNoResultCallback();
+          }
+        }.bind(this);
+        request.send();
+      }
+    
+      oncallback(newData){
+        console.log("newData");
+        console.log(newData);
+      }
+
+      onNoResultCallback(){
+        console.log("onNoResultCallback");
+      }
+
+    render(){
+        return(
+            <div>
+            <h1>hello</h1>
+            </div>
+        )
+    }
+}
+
+render (<App/>, window.document.getElementById("app"));
