d788cf66ee45dfdb04fd0296ae0c8593c0895280
PurnimaNaik <naikpurnima9@gmail.com>
2019-09-01 22:20:13 -0400

    styles started

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { render } from 'react-dom';
-
-
+import style from '../styles/index.css';
 
 class App extends React.Component {
   constructor() {
@@ -13,7 +12,7 @@ class App extends React.Component {
       packageNameInState: '',
       startDate: new Date(),
       endDate: new Date(),
-      response:"",
+      response: '',
     };
   }
   componentDidMount() {
@@ -24,13 +23,17 @@ class App extends React.Component {
     var request = new XMLHttpRequest();
     // https://api.npmjs.org/downloads/point/2019-06-01:2019-09-30/two-part-progress-circle
 
-
-    var formatedStartDate= this.formatDate(this.state.startDate);
-    var formatedEndDate= this.formatDate(this.state.endDate);
+    var formatedStartDate = this.formatDate(this.state.startDate);
+    var formatedEndDate = this.formatDate(this.state.endDate);
 
     request.open(
       'GET',
-      'https://api.npmjs.org/downloads/point/'+formatedStartDate+':'+formatedEndDate+'/'+this.state.packageNameInState,
+      'https://api.npmjs.org/downloads/point/' +
+        formatedStartDate +
+        ':' +
+        formatedEndDate +
+        '/' +
+        this.state.packageNameInState,
       true
     );
     request.onload = function() {
@@ -49,8 +52,8 @@ class App extends React.Component {
     console.log('response');
     console.log(response);
     this.setState({
-        responseInState:response,
-    })
+      responseInState: response,
+    });
   }
 
   onNoResultCallback() {
@@ -58,24 +61,24 @@ class App extends React.Component {
   }
 
   onSubmit(e) {
-    console.log("this.state.packageNameInState");
+    console.log('this.state.packageNameInState');
     console.log(this.state.packageNameInState);
     console.log(this.state.startDate);
     console.log(this.state.endDate);
     this.retrieveCount();
-}
+  }
 
- formatDate(date) {
+  formatDate(date) {
     var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
 
     return [year, month, day].join('-');
-}
+  }
 
   setStartDate(date) {
     this.setState({
@@ -90,60 +93,62 @@ class App extends React.Component {
   }
 
   render() {
-      
     return (
-      <div>
+      <div className="container">
         <h1>hello</h1>
         <form className="npm-count-form">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="package name"
+            onChange={evt => {
+              this.setState({ packageNameInState: evt.target.value });
+            }}
+          />
+          <h4>Select start date</h4> <h4>Select end date</h4>
+          <>
+            <DatePicker
+              selectsStart
+              selected={this.state.startDate}
+              onChange={this.setStartDate.bind(this)}
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              maxDate={new Date()}
+              isClearable
+            />
 
-        <input type="text" className="form-control" placeholder="package name" onChange={(evt) => {this.setState({ packageNameInState:evt.target.value }); }} />
-        
-        <h4>Select start date</h4> <h4>Select end date</h4>
-        <>
-        <DatePicker
-        selectsStart
-          selected={this.state.startDate}
-          onChange={this.setStartDate.bind(this)}
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          maxDate={new Date()}
-          isClearable
-        />
-       
-        <DatePicker
-        selectsEnd
-          selected={this.state.endDate}
-          onChange={this.setEndDate.bind(this)}
-          endDate={this.state.endDate}
-          startDate={this.state.startDate}
-          minDate={this.state.startDate}
-          maxDate={new Date()}
-          isClearable
-        />
-</>
+            <DatePicker
+              selectsEnd
+              selected={this.state.endDate}
+              onChange={this.setEndDate.bind(this)}
+              endDate={this.state.endDate}
+              startDate={this.state.startDate}
+              minDate={this.state.startDate}
+              maxDate={new Date()}
+              isClearable
+            />
+          </>
         </form>
 
-        <button type="button" onClick={this.onSubmit} className="btn">Get Count</button>
+        <button type="button" onClick={this.onSubmit} className="btn">
+          Get Count
+        </button>
 
-{this.state.responseInState?
-<div>
-<h3>Results</h3>
-        <h3>{this.state.responseInState.downloads}</h3>
-        <h3>{this.state.responseInState.start}</h3>
-        <h3>{this.state.responseInState.end}</h3>
-        <h3>{this.state.responseInState.package}</h3>
-</div>
-:
-null}
-
-
-
+        {this.state.responseInState ? (
+          <div>
+            <h3>Results</h3>
+            <h3>{this.state.responseInState.downloads}</h3>
+            <h3>{this.state.responseInState.start}</h3>
+            <h3>{this.state.responseInState.end}</h3>
+            <h3>{this.state.responseInState.package}</h3>
+          </div>
+        ) : null}
       </div>
     );
   }
 }
 
+
+
+
 render(<App />, window.document.getElementById('app'));
-
-
-// {downloads: 18, start: "2019-08-05", end: "2019-09-01", package: "two-part-progress-circle"}
